26773f3486fd57e07b0560e82dcc0df8dfa6b1e7
Fatih Zengin <fatihzengin654@outlook.com>
2024-08-24 13:13:03 +0300

    default response code for services set to 200

--- app/Services/ServiceResponse.php
+++ app/Services/ServiceResponse.php
@@ -9,7 +9,7 @@ class ServiceResponse
     public $status;
     public $data;
 
-    public function __construct($data, $status)
+    public function __construct($data, $status = 200)
     {
         $this->data = $data;
         $this->status = $status;
