5d0b8c40f2fba3df21621997766f169785e02683
Fatih Zengin <fatihzengin654@outlook.com>
2024-09-11 23:33:41 +0300

    added updateprofile to userserviceinterface

--- app/Services/Abstract/UserServiceInterface.php
+++ app/Services/Abstract/UserServiceInterface.php
@@ -3,9 +3,11 @@
 namespace App\Services\Abstract;
 
 use App\Http\Requests\User\UpdateAvatarRequest;
+use App\Http\Requests\User\UpdateUserProfileRequest;
 use App\Services\ServiceResponse;
 
 interface UserServiceInterface
 {
     public function updateAvatar(UpdateAvatarRequest $request): ServiceResponse;
+    public function updateProfile(UpdateUserProfileRequest $request): ServiceResponse;
 }
